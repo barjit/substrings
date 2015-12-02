2ddeb2f12c5ff6178ed7f4644faaae6a6b9a3cb1
jitterboggle <jeffreybartels@gmail.com>
2015-12-02 19:56:28 +1100

    add extra iterator

--- substrings.rb
+++ substrings.rb
@@ -1,18 +1,19 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(input, dictionary)
-  
-  words = input.downcase.scan(/[\w']+/)
+
+  words = input.downcase.split
   frequencies = Hash.new(0)
-  
-  search = words.map do |word|
-    if dictionary.include?(word)
-      frequencies[word] += 1
+
+  words.map do |word|
+    dictionary.map do |dict|
+      if word.include?(dict)
+        frequencies[dict] += 1
+      end
     end
   end
 
-
-  frequencies.each {|word, count| puts "#{word} : #{count}"}
+  frequencies
 
 end
 
